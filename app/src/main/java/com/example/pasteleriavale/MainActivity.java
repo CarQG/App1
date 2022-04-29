8321bb0c8c59c557c3f44b29eb38bd01850f1305
CarQG <cquijanogarcia.402@gmail.com>
2022-04-29 14:15:10 -0500

    first commit

---
+++ app/src/main/java/com/example/pasteleriavale/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.pasteleriavale;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TimerTask carlos = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        Timer time = new Timer();
+        time.schedule(carlos,5000);
+    }
+}
